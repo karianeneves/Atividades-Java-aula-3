b881147d1efc5d6e3c32c574ef1cfe9137c8e7dc
kariane <karianeneves18@gmail.com>
2025-08-04 19:18:34 -0300

    Update cliente.java

--- Aula 3/atividade1/cliente.java
+++ Aula 3/atividade1/cliente.java
@@ -1,15 +1,16 @@
 public class Cliente {
-    String nome;
-    int id;
+    String Nome;
+    int Id;
 
     public Cliente(String nome, int id) {
-        this.nome = nome;
-        this.id = id;
+        Nome = nome;
+        Id = id;
     }
 
     public void exibirDados() {
         System.out.println("Detalhes do Cliente:");
-        System.out.println("Nome: " + nome);
-        System.out.println("ID: " + id);
+        System.out.println("Nome: " + Nome);
+        System.out.println("ID: " + Id);
     }
+
 }
