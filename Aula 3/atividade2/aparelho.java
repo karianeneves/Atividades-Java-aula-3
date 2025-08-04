1c96d1a06d5d279f959076d1e22c793441a2c233
kariane <karianeneves18@gmail.com>
2025-08-04 19:37:37 -0300

    aparelho.java

--- Aula 3/atividade2/aparelho.java
+++ Aula 3/atividade2/aparelho.java
@@ -2,12 +2,14 @@ public class Aparelho {
     String nomeAparelho;
     double cargaAtual;
 
-    public Aparelho(String nomeAparelho, double cargaAtual) {
-        this.nomeAparelho = nomeAparelho;
-        this.cargaAtual = cargaAtual;
+    public Aparelho(String novonomeAparelho, double novacargaAtual) {
+        nomeAparelho = novonomeAparelho;
+        cargaAtual = novacargaAtual;
     }
 
     public void usarAparelho() {
-        System.out.println("Usando o aparelho " + nomeAparelho + " com carga de " + cargaAtual + " kg.");
+        System.out.println("Usando o aparelho " + nomeAparelho + " com carga de "
+         + cargaAtual + " kg.");
     }
+
 }
