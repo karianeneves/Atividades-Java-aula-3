d42d70ccae03a2af7e136a9c4c0935c5585697fc
kariane <karianeneves18@gmail.com>
2025-08-04 19:41:41 -0300

    treino.java

--- Aula 3/atividade2/treino.java
+++ Aula 3/atividade2/treino.java
@@ -2,12 +2,13 @@ public class Treino {
     String nomeTreino;
     int duracaoMinutos;
 
-    public Treino(String nomeTreino, int duracaoMinutos) {
-        this.nomeTreino = nomeTreino;
-        this.duracaoMinutos = duracaoMinutos;
+    public Treino(String novonomeTreino, int novaduracaoMinutos) {
+        nomeTreino = novonomeTreino;
+        duracaoMinutos = novaduracaoMinutos;
     }
 
     public void iniciarTreino() {
         System.out.println("Iniciando treino de " + nomeTreino + " com duração " + duracaoMinutos + " minutos.");
     }
+
 }
